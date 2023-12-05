02ff607d26644393a3ffef793c9f1fdb618c5cb6
Axel <axel_nomore@hotmail.com>
2023-12-05 18:28:59 -0400

    refactor(oracle) add uwsgi

--- wsgi.py
+++ wsgi.py
@@ -6,10 +6,10 @@ import sys
 
 # sys.path.insert(0, "/home/seu-usuario/projetos/flask-test")
 # sys.path.insert(0, "E:/Desenvolvimento\python/py_flask_celery/")
-sys.path.append(os.path.dirname(__file__))
+# sys.path.append(os.path.dirname(__file__))
 
-from app import app
+from app import app as application
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=5002, debug=False)
-    app.run()
+    application.run(host="0.0.0.0", debug=False)
